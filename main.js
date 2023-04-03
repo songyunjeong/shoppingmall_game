eba860db1103a484f2f564ad092189e07c6a2f4a
songyunjeong <yungunaa@naver.com>
2023-04-03 16:48:33 +0900

    version 1

--- main.js
+++ main.js
@@ -20,11 +20,29 @@ function createHTMLString(item) {
   `;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter((item) => item[key] === value));
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', (event) => onButtonClick(event, items));
+}
+
 // main
 loadItems()
   .then((items) => {
-    console.log(items);
     displayItems(items);
-    // setEventListeners(items);
+    setEventListeners(items);
   })
   .catch(console.log);
