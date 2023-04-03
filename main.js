8987792072d364cb046189e53bc7e1098e12c153
songyunjeong <yungunaa@naver.com>
2023-04-03 15:27:17 +0900

    html css json

---
+++ main.js
@@ -0,0 +1,30 @@
+// Fetch the items from the JSON file
+function loadItems() {
+  return fetch('data/data.json')
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
+
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+  return `
+  <li class="item">
+    <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+    <span class="item__description">${item.gender}, ${item.size}</span>
+  </li>
+  `;
+}
+
+// main
+loadItems()
+  .then((items) => {
+    console.log(items);
+    displayItems(items);
+    // setEventListeners(items);
+  })
+  .catch(console.log);
